6a5f7f4323aa21c57ed6c86b148d4a540bb4169e
ricardo <ricardo.vargas.haro@uabc.edu.mx>
2023-09-19 01:14:21 -0700

    se actualizaron los nombres

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
         baraja.crearBaraja();
+        baraja.barajar();
         System.out.println("Cantidad de cartas: " + baraja.getCantidadDeCartas());
         System.out.println("Baraja:\n" + baraja);
 
@@ -15,8 +16,11 @@ public class Test {
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.add(jCarta, BorderLayout.CENTER);
+        frame.setLayout(new GridLayout(1, 0));
+        for (int i = 0; i < 5; i++) {
+            frame.add(new JCarta(baraja.removerCarta()));
+        }
+        // frame.add(jCarta);
         frame.setVisible(true);
 
 
